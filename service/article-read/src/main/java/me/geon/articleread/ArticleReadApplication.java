b638bd519852c4baeb998155e76503091bd9862d
mugeon <pos04167@kakao.com>
2025-01-25 19:37:51 +0900

    init msa package

---
+++ service/article-read/src/main/java/me/geon/articleread/ArticleReadApplication.java
@@ -0,0 +1,13 @@
+package me.geon.articleread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleReadApplication.class, args);
+    }
+
+}
---
+++ service/article/src/main/java/me/geon/artice/ArticleApplication.java
@@ -0,0 +1,13 @@
+package me.geon.artice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class, args);
+    }
+
+}
